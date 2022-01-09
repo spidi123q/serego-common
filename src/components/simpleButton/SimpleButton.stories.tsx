d153f337521a1952e9833021a45ce0c8e5eeb112
Suraj Kiran S <spidi123q@gmail.com>
2022-01-09 15:12:04 +0530

    contained-light button variant added

--- src/components/simpleButton/SimpleButton.stories.tsx
+++ src/components/simpleButton/SimpleButton.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react/types-6-0";
-import SimpleButton, { ISimpleButtonProps } from "./SimpleButton";
+import { ISimpleButtonProps, SimpleButton } from "./SimpleButton";
 
 export default {
   title: "Components/Button",
--- src/components/simpleButton/SimpleButton.tsx
+++ src/components/simpleButton/SimpleButton.tsx
@@ -3,27 +3,40 @@ import "./SimpleButton.scss";
 import Button, { ButtonProps } from "@mui/material/Button";
 import classNames from "classnames";
 
-export interface ISimpleButtonProps extends ButtonProps {
+export interface ISimpleButtonProps extends Omit<ButtonProps, "variant"> {
   marginRight?: boolean;
   marginLeft?: boolean;
+  variant?: IType;
 }
 
-export default function SimpleButton({
-  children,
-  marginRight,
-  marginLeft,
-  ...rest
-}: ISimpleButtonProps) {
+export const SimpleButton: React.FunctionComponent<ISimpleButtonProps> = (
+  props
+) => {
+  const { children, marginRight, marginLeft, variant, ...rest } = props;
   return (
     <span
       className={classNames("simple-button", {
         "simple-button__margin--right": marginRight,
         "simple-button__margin--left": marginLeft,
+        [`simple-button__variant--${variant}`]: variant,
       })}
     >
-      <Button variant="contained" {...rest}>
+      <Button variant={typeVariantMap[variant ?? "contained"]} {...rest}>
         {children}
       </Button>
     </span>
   );
-}
+};
+
+SimpleButton.defaultProps = {
+  variant: "contained",
+};
+
+const typeVariantMap: Record<IType, "text" | "outlined" | "contained"> = {
+  text: "text",
+  outlined: "outlined",
+  contained: "contained",
+  "contained-light": "text",
+};
+
+type IType = "text" | "contained" | "outlined" | "contained-light";
